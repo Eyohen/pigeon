3a384a20f84da9310a7e32dd9811179408961d09
Eyohen <henry.eyo2@gmail.com>
2025-03-18 14:03:30 +0100

    didnt do the url well

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
   const fetchPosts = async () => {
     try {
       const res = await axios.get(`${URL}/api/posts`);
-      console.log(res.data);
+      console.log("see blog posts henry",res.data);
       setPosts(res.data);
     } catch (error) {
       console.log(error);
